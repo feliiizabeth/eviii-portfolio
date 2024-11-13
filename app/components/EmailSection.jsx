127bd41b730759930671f9c52a05739f0bb37800
Elizabeth Vazquez <ev2070@nyu.edu>
2024-11-13 16:54:00 -0500

    Update Email and Project Card styling and NavBar link paths

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -44,7 +44,7 @@ const EmailSection = () => {
   return (
     <section
       id="contact"
-      className="grid md:grid-cols-2 mb-12 md:mb-12 pt-22 sm:pt-23 lg:pt-28 pb-24 gap-4 relative"
+      className="grid md:grid-cols-2 mb-12 pt-22 sm:pt-23 lg:pt-28 pb-24 gap-4 relative"
     >
       {/* Blurry circle */}
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -25,7 +25,7 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, projectUrl }) => {
               </Link>
             )}
 
-            {/* Preview link icon */}
+            {/* Project link icon */}
             <Link
               href={projectUrl}
               className="h-14 w-14 border-2 rounded-full border-[#ADB7BE] hover:border-white flex items-center justify-center"
@@ -39,7 +39,9 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, projectUrl }) => {
       {/* Project details */}
       <div className="text-white rounded-b-xl mt-3 bg-[#181818] py-6 px-4">
         <h5 className="text-xl font-semibold mb-2">{title}</h5>
-        <p className="text-[#ADB7BE]">{description}</p>
+        <p className="text-[#ADB7BE] lg:line-clamp-5 md:line-clamp-4 line-clamp-3">
+          {description}
+        </p>
       </div>
     </div>
   );
