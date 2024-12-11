7956577c8ed4b95259e451dba1ae714952a0b4f8
Elizabeth Vazquez <ev2070@nyu.edu>
2024-12-10 21:18:10 -0500

    Update font to Outfit

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,18 @@
-import { Inter } from "next/font/google";
+import { Outfit } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const outfit = Outfit({ subsets: ["latin"] });
 
 export const metadata = {
   title: "EVIII Portfolio",
-  description: "Showcasing my expertise in programming and art through a responsive portfolio. Explore my technical and creative projects, designed to attract potential employers and collaborators with intuitive navigation and clean design.",
+  description:
+    "Showcasing my expertise in programming and art through a responsive portfolio. Explore my technical and creative projects, designed to attract potential employers and collaborators.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={outfit.className}>
         {/* Dummy script tag to prevent Firefox FOUC */}
         <script>0</script>
         {children}
