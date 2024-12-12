47ab230e66c035c49047abafdd971eb66fa01c30
Elizabeth Vazquez <ev2070@nyu.edu>
2024-12-12 17:13:04 -0500

    Center footer content horizontally

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent border-b-transparent text-white">
-      <div className="container p-12 flex justify-between">
+      <div className="container p-12 flex items-center justify-between mx-auto">
         <span>LOGO</span>
         <p className="text-slate-600">All rights reserved.</p>
       </div>
